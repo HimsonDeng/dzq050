b455fcf7a4b2d22ef430dcbb58b4c8963b0d4d5b
HimsonDeng <118723354+HimsonDeng@users.noreply.github.com>
2022-11-28 21:14:17 +0800

    Add files via upload

---
+++ js/controls.js
@@ -0,0 +1,21 @@
+$(function() {
+    //Simple filter controls
+    $('.simplefilter li').click(function() {
+        $('.simplefilter li').removeClass('active');
+        $(this).addClass('active');
+    });
+    //Multifilter controls
+    $('.multifilter li').click(function() {
+        $(this).toggleClass('active');
+    });
+    //Shuffle control
+    $('.shuffle-btn').click(function() {
+        $('.sort-btn').removeClass('active');
+    });
+    //Sort controls
+    $('.sort-btn').click(function() {
+        $('.sort-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+/*-- editor:dzq050	first change/check:2022.11.28 17:08   for course learning homework use --*/
